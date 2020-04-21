05d3a752d2f9aaf63e9af6fe09da3a16275aa9b1
solidw <solidw@naver.com>
2020-04-22 04:28:20 +0900

    feat: Add Route with pageComponents

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import styled from "styled-components";
 
-function App() {
+import HomePage from "./pages/HomePage";
+import IndexPage from "./pages/IndexPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import CoursePage from "./pages/CoursePage";
+import TheoryPage from "./pages/TheoryPage";
+import PracticePage from "./pages/PracticePage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Main>
+        <MainWrapper>
+          <Switch>
+            <Route exact path="/" component={IndexPage} />
+            <Route path="/home" component={HomePage} />
+            <Route path="/course" component={CoursePage} />
+            <Route path="/theory" component={TheoryPage} />
+            <Route path="/practice" component={PracticePage} />
+            <Route path="/register" component={RegisterPage} />
+            <Route path="/login" component={LoginPage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </MainWrapper>
+      </Main>
+    </>
   );
-}
+};
 
+const MainWrapper = styled.div`
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+`;
+
+const Main = styled.main`
+  padding: 2em 0px;
+  height: 100%;
+`;
 export default App;
