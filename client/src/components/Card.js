6a117789afd650931e92a841aaa2ae16b4036877
solidw <solidw@naver.com>
2020-04-22 04:30:46 +0900

    feat: Add Skeleton React Functional Component using code-snippets which is vscode function

---
+++ client/src/components/Card.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+const Card = () => {
+  return <CardWrapper></CardWrapper>;
+};
+
+const CardWrapper = styled.div``;
+export default Card;
