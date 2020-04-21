6aad4784d1d4f18799e2208adebbfd0876a20f95
solidw <solidw@naver.com>
2020-04-22 04:28:36 +0900

    feat: Add color palette

---
+++ client/src/lib/styles/palette.js
@@ -0,0 +1,16 @@
+const palette = {
+  /* pastel */
+  pastel0: "#B29DD9",
+  pastel1: "#FDFD98",
+  pastel2: "#FE6B64",
+  pastel3: "#77DD77",
+  pastel4: "#779ECB",
+
+  /* white */
+  white: "#FFFFFF",
+  black: "#000000",
+  classicBlue: "#0F4C81",
+  darkBlue: "#2b303b",
+};
+
+export default palette;
