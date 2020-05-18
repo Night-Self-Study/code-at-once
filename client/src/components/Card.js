1fd91dc22b56e4f54645185886703990e60d44e8
solidw <solidw@naver.com>
2020-05-19 03:51:37 +0900

    feat: Add attribut in card it need

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,19 +1,65 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import palette from "../lib/styles/palette";
 
-const Card = ({ cardWidth }) => {
-  return <CardWrapper cardWidth={cardWidth}>CARD</CardWrapper>;
+import noImage from "../lib/asset/noimage.jpg";
+import NoDecorationLink from "./NoDecorationLink";
+
+const errorPath = "/error";
+
+const Card = ({
+  path = errorPath,
+  thumbnail = noImage,
+  level = "초급",
+  title = "title을 불러오지 못했습니다.",
+  author = "author를 불러오지 못했습니다.",
+  detail = "",
+}) => {
+  useEffect(() => {
+    console.log(path);
+  });
+
+  return (
+    <CardWrapper>
+      <NoDecorationLink to="/error">
+        <img alt="dummyImage_alt" src={thumbnail} />
+        {console.log(thumbnail)}
+        <div className="level">[{level}]</div>
+        <div className="title">{title}</div>
+        <div className="author">{author}</div>
+        <div className="detail">
+          {detail.score}
+          {detail.time}
+          {detail.numberOfLecture}
+        </div>
+      </NoDecorationLink>
+    </CardWrapper>
+  );
 };
 
 const CardWrapper = styled.div`
-  width: ${(props) => props.cardWidth}vw;
-  padding: 7vh;
-
   text-align: center;
-
+  margin: 10px 20px;
   border: 1px solid black;
   border-radius: 8px;
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+
+  img {
+    width: 100%;
+    max-width: 15vw;
+
+    border-radius: 8px;
+    overflow: hidden;
+  }
+  .level,
+  .title,
+  .author {
+    padding: 5px 0px;
+  }
+
+  .author {
+    font-size: 10px;
+    color: gray;
+  }
 `;
 export default Card;
