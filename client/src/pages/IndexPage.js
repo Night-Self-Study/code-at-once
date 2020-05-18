67127398278e18b7e748a665b7a00673a672d858
solidw <solidw@naver.com>
2020-05-19 03:52:00 +0900

    feat: Add dummy card with map func

--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -3,16 +3,47 @@ import styled from "styled-components";
 import Input from "../components/SearchBar";
 import Card from "../components/Card";
 
+import dummyImage from "../lib/asset/library.jpg";
+import palette from "../lib/styles/palette";
+
+import HamburgerMenuIcon from "../components/HamburgerMenuIcon";
+
 const IndexPage = () => {
   const [searchBar, setSearchBar] = useState("");
-
+  const makeDummyCard = () => {
+    const dummy = [];
+    for (let count = 0; count < 4; count++) {
+      dummy.push({
+        path: "/course/web/1",
+        title: `Course${count}`,
+        thumbnail: count % 3 === 1 ? dummyImage : undefined,
+        level: count % 3 === 1 ? "초급" : "중급",
+        author: `author${count}`,
+      });
+    }
+    return dummy;
+  };
+  const dummy = makeDummyCard();
   return (
     <IndexWrapper>
       <Input type="container-1" inputWidth="50%" />
-      <section className="section1">
-        <Card cardWidth="7" />
-        <Card cardWidth="7" />
-        <Card cardWidth="7" />
+      <header>
+        <HamburgerMenuIcon />
+        <span>web</span>
+        <span>ai</span>
+        <span>OS</span>
+        <span>Data Structure</span>
+      </header>
+      <section className="courses">
+        {dummy.map((item) => (
+          <Card
+            path={item.path}
+            title={item.title}
+            thumbnail={item.thumbnail}
+            level={item.level}
+            author={item.author}
+          />
+        ))}
       </section>
     </IndexWrapper>
   );
@@ -23,7 +54,19 @@ const IndexWrapper = styled.div`
   align-items: center;
   flex-direction: column;
   flex-wrap: wrap;
-  .section1 {
+
+  header {
+    display: flex;
+    align-self: stretch;
+    justify-content: space-around;
+    align-items: center;
+    padding: 20px 0px;
+    margin: 40px 0px;
+    background: ${palette.classicBlue};
+    color: white;
+  }
+
+  .courses {
     width: 100%;
     margin: 40px 0px;
     display: flex;
