8430f557e53f6326ec147c0c192c1f957c7d93b3
solidw <solidw@naver.com>
2020-05-19 03:50:42 +0900

    feat: Add Link with Nodecoration and color attr

---
+++ client/src/components/NoDecorationLink.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import palette from "../lib/styles/palette";
+
+const NoDecorationLink = styled(Link)`
+  text-decoration: none;
+  font-weight: 200;
+  color: ${(props) => (props.color ? props.color : palette.black)};
+`;
+
+export default NoDecorationLink;
