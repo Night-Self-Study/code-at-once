0ca46297da67da54e21f263a88bd2aebd6fe1f51
solidw <solidw@naver.com>
2020-05-20 02:38:50 +0900

    feat: Add custom hooks can treat as boolean values

---
+++ client/src/lib/hooks/useToggle.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+const useToggle = (initialValue = false) => {
+  if (typeof initialValue !== "boolean") {
+    throw Error("useToggle: The value is not Boolean type");
+  }
+
+  const [value, setValue] = useState(initialValue);
+
+  return [
+    value,
+    {
+      set: setValue,
+      toggle: () => setValue((flag) => !flag),
+    },
+  ];
+};
+
+export default useToggle;
