cd3be2a50606f01d1d17e28d9332fb32a5064efd
solidw <solidw@naver.com>
2020-05-23 18:35:24 +0900

    feat: Add dummyDB to use at web

---
+++ client/src/lib/dummyCourse.js
@@ -0,0 +1,28 @@
+import noImage from "lib/assets/noimage.jpg";
+
+export const dummyWeb = [
+  {
+    thumbnail: noImage,
+    title: "3일 안에 배우는 HTML",
+    author: "최수연",
+    level: "초급",
+  },
+  {
+    thumbnail: noImage,
+    title: "HTML5로 혁신적인 웹페이지 만들기",
+    author: "고태완",
+    level: "중급",
+  },
+  {
+    thumbnail: noImage,
+    title: "RESTful API를 이용한 게시판",
+    author: "김지혜",
+    level: "고급",
+  },
+  {
+    thumbnail: noImage,
+    title: "CSS 기초 분석",
+    author: "최수연",
+    level: "초급",
+  },
+];
