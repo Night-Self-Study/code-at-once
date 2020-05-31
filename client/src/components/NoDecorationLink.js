b0150bf48b52965c9dc56ed65222f7716e3e441e
solidw <solidw@naver.com>
2020-05-31 22:05:18 +0900

    feat: Add css width, height 100% in LinkWrapper

--- client/src/components/NoDecorationLink.js
+++ client/src/components/NoDecorationLink.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import palette from "lib/styles/palette";
 
 const NoDecorationLink = styled(Link)`
+  width: 100%;
+  height: 100%;
   text-decoration: none;
   font-weight: 200;
   color: ${(props) => (props.color ? props.color : palette.black)};
