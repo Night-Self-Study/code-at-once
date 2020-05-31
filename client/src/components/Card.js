74754414b0729f11f4a85c54adee103945a8db85
solidw <solidw@naver.com>
2020-05-31 22:04:47 +0900

    feat: Add isLong attr in Card.js

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -8,6 +8,7 @@ import NoDecorationLink from "components/NoDecorationLink";
 const errorPath = "/error";
 
 const Card = ({
+  isLong = false,
   path = errorPath,
   thumbnail = noImage,
   level = "초급",
@@ -15,10 +16,27 @@ const Card = ({
   author = "author를 불러오지 못했습니다.",
   detail = "",
 }) => {
-  return (
+  return isLong ? (
+    <LongCardWrapper>
+      <NoDecorationLink to={path}>
+        <img alt="dummyImage_alt" src={thumbnail} />
+        <div className="description">
+          <div className="level">[{level}]</div>
+          <div className="title">{title}</div>
+          <div className="author">{author}</div>
+          <div className="detail">
+            {detail.score}
+            {detail.time}
+            {detail.numberOfLecture}
+          </div>
+        </div>
+      </NoDecorationLink>
+    </LongCardWrapper>
+  ) : (
     <CardWrapper>
       <NoDecorationLink to={path}>
         <img alt="dummyImage_alt" src={thumbnail} />
+
         <div className="level">[{level}]</div>
         <div className="title">{title}</div>
         <div className="author">{author}</div>
@@ -55,6 +73,33 @@ const CardWrapper = styled.div`
   .author {
     font-size: 10px;
     color: gray;
+  }
+`;
+
+const LongCardWrapper = styled.div`
+  margin: 10px 20px;
+  border: 1px solid black;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+  padding: 10px;
+  ${NoDecorationLink} {
+    display: flex;
+  }
+  img {
+    width: 100%;
+    max-width: 15vw;
+
+    border-radius: 8px;
+    overflow: hidden;
+  }
+  .description {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-left: 15px;
+    div {
+      margin: 5px 0px;
+    }
   }
 `;
 export default Card;
