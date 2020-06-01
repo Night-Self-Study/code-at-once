e0c4baf6d333404890c0f49a7a0334ec9a70f80c
solidw <solidw@naver.com>
2020-06-01 16:09:35 +0900

    feat: Add category router for routing each categories web, os, etc

---
+++ client/src/containers/Categories/CategoryRouter.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+import { Route } from "react-router-dom";
+
+import CategoryIndex from "./CategoryIndex";
+import CategoryList from "./CategoryList";
+
+const CategoryRouter = ({ match }) => {
+  return (
+    <>
+      <Route exact path={match.path} component={CategoryIndex} />
+      <Route path={`${match.path}/:category`} component={CategoryList} />
+    </>
+  );
+};
+
+export default CategoryRouter;
