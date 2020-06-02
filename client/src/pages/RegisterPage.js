e09b10568e0322d3154230bd29cbed5bcc3c15d7
solidw <solidw@naver.com>
2020-06-02 21:27:45 +0900

    refactor: Move Register logic to containers/Auth/Register.js

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,34 +1,8 @@
 import React from "react";
-import styled from "styled-components";
-import CustomForm from "components/CustomForm";
-import Button from "components/Button";
+import Register from "containers/Auth/Register";
 
 const RegisterPage = () => {
-  return (
-    <RegisterPageWrapper>
-      <div className="header">회원가입</div>
-      <CustomForm
-        data={["ID", "Password", "Password 확인", "이름", "E-Mail"]}
-      />
-      <Button>회원가입</Button>
-    </RegisterPageWrapper>
-  );
+  return <Register />;
 };
 
-const RegisterPageWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 50%;
-  margin: 0 auto;
-
-  .header {
-    text-align: center;
-    margin: 20px 0px;
-    font-size: 28px;
-  }
-
-  Button {
-    text-algin: center;
-  }
-`;
 export default RegisterPage;
