f195b3bfa9d5fd05a6c905c1af1f188a4ddfb8ae
solidw <solidw@naver.com>
2020-06-02 21:06:06 +0900

    refactor: Change Input react component to styled-component

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,11 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-const Input = () => {
-  return <InputWrapper />;
-};
-
-const InputWrapper = styled.input`
+const Input = styled.input`
   border-radius: 5px;
   border: none;
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
