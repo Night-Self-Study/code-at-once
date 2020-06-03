21c8f2d114d9f1219744d9be5db2258ba6200bc4
solidw <solidw@naver.com>
2020-06-04 01:08:55 +0900

    feat: Change blur value 4px to 10px

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Input = styled.input`
   border-radius: 5px;
   border: none;
-  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+  box-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.2);
   font-weight: 200px;
   padding: 20px 30px;
 `;
