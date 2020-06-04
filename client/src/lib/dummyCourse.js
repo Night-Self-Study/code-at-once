30e9a77f83508a9747ad367e44784a16942e35a2
solidw <solidw@naver.com>
2020-06-05 02:23:16 +0900

    feat: Add id value in dummyCourse

--- client/src/lib/dummyCourse.js
+++ client/src/lib/dummyCourse.js
@@ -2,24 +2,28 @@ import noImage from "lib/assets/noimage.jpg";
 const dummyCourse = {
   web: [
     {
+      id: "1",
       thumbnail: noImage,
       title: "3일 안에 배우는 HTML",
       author: "최수연",
       level: "초급",
     },
     {
+      id: "2",
       thumbnail: noImage,
       title: "HTML5로 혁신적인 웹페이지 만들기",
       author: "고태완",
       level: "중급",
     },
     {
+      id: "3",
       thumbnail: noImage,
       title: "RESTful API를 이용한 게시판",
       author: "김지혜",
       level: "고급",
     },
     {
+      id: "4",
       thumbnail: noImage,
       title: "CSS 기초 분석",
       author: "최수연",
@@ -28,24 +32,28 @@ const dummyCourse = {
   ],
   os: [
     {
+      id: "1",
       thumbnail: noImage,
       title: "Linux를 알아보자",
       author: "고태완",
       level: "초급",
     },
     {
+      id: "2",
       thumbnail: noImage,
       title: "Windows에서 Linux 사용하기",
       author: "고태완",
       level: "중급",
     },
     {
+      id: "3",
       thumbnail: noImage,
       title: "OS 실습",
       author: "김지혜",
       level: "고급",
     },
     {
+      id: "4",
       thumbnail: noImage,
       title: "시스템콜 실습",
       author: "최수연",
