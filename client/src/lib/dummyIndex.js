d00ee33367c2dc9671cdee95a98c75e31a36d6ef
solidw <solidw@naver.com>
2020-06-05 04:13:45 +0900

    feat: Add dummyIndex

---
+++ client/src/lib/dummyIndex.js
@@ -0,0 +1,16 @@
+const dummyIndex = [
+  {
+    title: "HTML",
+    contents: ["HTML이란?", "HTML5", "tag 알아보기"],
+  },
+  {
+    title: "CSS",
+    contents: ["CSS란?", "CSS 시작하기", "CSS Trick"],
+  },
+  {
+    title: "JavaScript",
+    contents: ["JavaScript란?", "JavaScript 시작하기", "동적인 웹페이지"],
+  },
+];
+
+export default dummyIndex;
