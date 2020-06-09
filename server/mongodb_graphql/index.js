b32a1c1a5d38ef71075595e25c70df9962928fe5
SuyeonChoi <tndus970120@gmail.com>
2020-06-10 05:51:32 +0900

    Add MongoDB

--- server/mongodb_graphql/index.js
+++ server/mongodb_graphql/index.js
@@ -1,6 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
 const app = express();
 const port = 3000;
+// MongoDB connect (password: nightselfstudy)
+const uri = 'mongodb+srv://user_sy:nightselfstudy@code-at-once-mgoje.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority'
+mongoose.Promise = global.Promise; //비동기처리
+mongoose.connect(uri, { useNewUrlParser: true }); 
 
 app.get('/', (req, res) => {
     res.json({
