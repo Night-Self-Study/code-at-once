11cd8d9b2d8a63c9fe560fd220a3a12080b22eff
SuyeonChoi <tndus970120@gmail.com>
2020-06-10 14:41:28 +0900

    Delete django

--- server/mongodb_graphql/graphql/resolvers.js
+++ server/mongodb_graphql/graphql/resolvers.js
@@ -1,8 +1,12 @@
 import User from '../models/user';
+import { SingleFieldSubscriptionsRule } from 'graphql';
 export const resolvers = {
     Query: {
         async allUser() {
             return await User.find();
+        },
+        async getUser(root, { id, password }){
+            return await User.fi;
         }
     },
     Mutation: {
