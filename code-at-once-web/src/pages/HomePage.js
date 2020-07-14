1559ec2576967630d6f186f3a3923dd7f9502537
solidw <solidw@naver.com>
2020-07-15 02:03:25 +0900

    refactor: Move files to proper folders

--- code-at-once-web/src/pages/HomePage.js
+++ code-at-once-web/src/pages/HomePage.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Box } from '@material-ui/core';
 
 import Introduce from 'components/home/Introduce';
-import SubjectContainer from 'containers/learn/SubjectContainer';
+import SubjectContainer from 'containers/subject/SubjectContainer';
 
 const HomePage = () => {
 	return (
