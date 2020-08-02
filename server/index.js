8032f0c52727ddd0367d5138ab6224c512ddcf80
SuyeonChoi <tndus970120@gmail.com>
2020-08-02 18:21:37 +0900

    FIX:Redis server running inside the Docker container connection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 var redis = require("redis"),
-    client = redis.createClient();
+    client = redis.createClient({ 
+        host: process.env.REDIS_HOST || '192.168.99.100',
+        port : 6379, 
+        db : 0,
+    });
+
 
 client.on("error", function (err) {
     console.log("Error " + err);
