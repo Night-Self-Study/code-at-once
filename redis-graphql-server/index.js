c1f64c4126abd2e0365901c5bd002400f911b60c
SuyeonChoi <tndus970120@gmail.com>
2020-08-05 04:10:48 +0900

    ADD graphql settings in index.js

--- redis-graphql-server/index.js
+++ redis-graphql-server/index.js
@@ -1,6 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { graphqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
+
+import typeDefs from './schema';
+import resolvers from './ resolvers';
+
+export const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+})
 
 const app = express();
 
