66c4a8d169c06b51197553bef61b97ee57ed0f13
solidw <solidw@naver.com>
2020-08-27 01:35:44 +0900

    feat: Align center in Loading component

--- code-at-once-web/src/components/Loading.js
+++ code-at-once-web/src/components/Loading.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { makeStyles, CircularProgress } from '@material-ui/core';
 
+const progressSize = 64;
+
 const useStyles = makeStyles({
 	root: {
 		position: 'absolute',
 		top: '50%',
 		left: '50%',
+		marginTop: ~~(progressSize / 2) * -1,
+		marginLeft: ~~(progressSize / 2) * -1,
 	},
 });
 
 export default function Loading() {
 	const classes = useStyles();
-	return <CircularProgress size='64px' className={classes.root} />;
+	return (
+		<CircularProgress size={`${progressSize}px`} className={classes.root} />
+	);
 }
