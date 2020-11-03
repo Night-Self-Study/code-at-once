e8ef01371cdeb32926eb32505354a46aae5bc2cb
solidw <solidw@naver.com>
2020-11-04 00:52:00 +0900

    feat: Add descrpition example

--- code-at-once-web/src/containers/challenge/ChallengeContainer.js
+++ code-at-once-web/src/containers/challenge/ChallengeContainer.js
@@ -47,6 +47,10 @@ const dummyChallengeToDemo = {
 	challengeDescription: [
 		`책 이름과 책에 대한 번호가 input으로 주어질 때, 번호가 가장 작은 책에 대해서, 해당 책 이름과 책의 번호를 출력하시오.`,
 	],
+	inputDescription: ['책 이름과 책 번호가 입력된다.'],
+	outputDescription: ['책 이름과 책 번호가 출력된다.'],
+	inputExample: [1, 2, 3, 4],
+	outputExample: [2, 4, 6, 8],
 };
 
 export default function ChallengeContainer({ match }) {
