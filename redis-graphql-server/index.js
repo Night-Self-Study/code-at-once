8524d5f1a096b8ffb6921df40e4ffee3d14762c2
SuyeonChoi <tndus970120@gmail.com>
2020-11-15 17:17:23 +0900

    fix createUser mutation

--- redis-graphql-server/index.js
+++ redis-graphql-server/index.js
@@ -1,29 +1,27 @@
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
-import redis from 'redis';
-import bluebird from 'bluebird';
-import typeDefs from './schema.js';
-import resolvers from './resolvers.js';
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import redis from "redis";
+import bluebird from "bluebird";
+import typeDefs from "./schema.js";
+import resolvers from "./resolvers.js";
 
 //Connect with Redis server
 const client = redis.createClient({
-    host: process.env.REDIS_HOST || '172.17.0.1',
-    port: 6379,
-    db: 0,
+  host: process.env.REDIS_HOST || "172.17.0.1",
+  port: 6379,
+  db: 0,
 });
 
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 
-client.on("error", err => {
-    console.log("Error " + err);
+client.on("error", (err) => {
+  console.log("Error " + err);
 });
-
 
 const app = express();
 //resolvers can access through client
 const server = new ApolloServer({ typeDefs, resolvers, context: { client } });
 server.applyMiddleware({ app });
-
 
 app.listen(3000);
--- redis-graphql-server/resolvers.js
+++ redis-graphql-server/resolvers.js
@@ -1,84 +1,91 @@
-import { stderr, stdout } from 'process';
-import fs from 'fs';
-import util from 'util';
-import child_process from 'child_process';
-
 //resolvers.js
 export default {
-    Query: {
-        getProblem: async (parent, { id }, { client }) => {
-            try {
-                const key = "Problem:" + id;
-                let problem = await client.hgetallAsync(key);
-                problem["problemDescription"] = [problem["problemDescription"]];
-                problem["inputDescription"] = [problem["inputDescription"]];
-                problem["outputDescription"] = [problem["outputDescription"]];
-                problem["inputExample"] = [problem["inputExample"]];
-                problem["outputExample"] = [problem["outputExample"]];
-                return problem;
-            } catch (e) {
-                return e;
-            }
-        }
+  Query: {
+    getProblem: async (parent, { id }, { client }) => {
+      try {
+        const key = "Problem:" + id;
+        let problem = await client.hgetallAsync(key);
+        problem["problemDescription"] = [problem["problemDescription"]];
+        problem["inputDescription"] = [problem["inputDescription"]];
+        problem["outputDescription"] = [problem["outputDescription"]];
+        problem["inputExample"] = [problem["inputExample"]];
+        problem["outputExample"] = [problem["outputExample"]];
+        return problem;
+      } catch (e) {
+        return e;
+      }
     },
+  },
 
-    Mutation: {
-        createUser: async (parent, { key, input }, { client }) => {
-            try {
-                await client.hmset(key + ":" + input.id, input);
-                return client.hgetallAsync(key + ":" + input.id);
-            } catch (e) {
-                console.log(e);
-                return false;
-            }
-        },
-        createProblem: async (parent, { id, input }, { client }) => {
-            try {
-                await client.hmset("Problem:" + id, {
-                    title: input.title,
-                    problemDescription: input.problemDescription.join(),
-                    inputDescription: input.inputDescription.join(),
-                    outputDescription: input.outputDescription.join(),
-                    inputExample: input.inputExample.join(),
-                    outputExample: input.outputExample.join()
-                });
-                return true;
-            } catch (e) {
-                return false;
-            }
-        },
-        markUserCode: async (parent, { id, input }, { client }) => {
-            try {
-                await client.hmset("Submission" + id, input);
-                let filename;
-                if (input.language === 'python') {
-                    filename = id + '_' + input.problemId + '_' + input.userId + '.py';
-                    fs.writeFileSync(filename, input.sourceCode);
-                } else if (input.language === 'java') {
-                    filename = id + '_' + input.problemId + '_' + input.userId + '.java';
-                    fs.writeFileSync(filename, input.sourceCode);
-                }
-                console.log(filename);
-                const exec = util.promisify(child_process.exec);
-                let command = '/home/ubuntu/code-at-once/domjudge-7.2.0/submit -y -p firstbook -l ' + input.language + ' ' + filename;
-                let { stdout, stderr } = await exec(command);
-                const [, submit_id] = stderr.split('id = s');
-
-                //set submit_id command temporarily
-                command = 'python3 /home/ubuntu/code-at-once/analyze_submission/mapping_not_correct.py ' + (submit_id - 1).toString();
-                const { stdout: cmdout, stderr: cmderr } = await exec(command);
-                // console.log('stdout:', cmdout);
-                // console.error('stderr:', cmderr);
-                const result = cmdout.split('\n');
-
-                return {
-                    "problemId": input.problemId, "userId": input.userId, "language": input.language,
-                    "submitAt": new Date().toISOString(), "resultType": result[0], "message": result[1]
-                };
-            } catch (e) {
-                console.log(e);
-                return false;
-            }
+  Mutation: {
+    createUser: async (parent, { key, input }, { client }) => {
+      try {
+        await client.hmset(key + ":" + input.id, input);
+        return client.hgetallAsync(key + ":" + input.id);
+      } catch (e) {
+        console.log(e);
+        return false;
+      }
+    },
+    createProblem: async (parent, { id, input }, { client }) => {
+      try {
+        await client.hmset("Problem:" + id, {
+          title: input.title,
+          problemDescription: input.problemDescription.join(),
+          inputDescription: input.inputDescription.join(),
+          outputDescription: input.outputDescription.join(),
+          inputExample: input.inputExample.join(),
+          outputExample: input.outputExample.join(),
+        });
+        return true;
+      } catch (e) {
+        return false;
+      }
+    },
+    markUserCode: async (parent, { id, input }, { client }) => {
+      try {
+        await client.hmset("Submission" + id, input);
+        const fs = require("fs");
+        let filename;
+        if (input.language === "python") {
+          filename = id + "_" + input.problemId + "_" + input.userId + ".py";
+          fs.writeFileSync(filename, input.sourceCode);
+        } else if (input.language === "java") {
+          filename = id + "_" + input.problemId + "_" + input.userId + ".java";
+          fs.writeFileSync(filename, input.sourceCode);
         }
-    }
-}
+
+        const util = require("util");
+        const exec = util.promisify(require("child_process").exec);
+        let command =
+          "/home/ubuntu/code-at-once/domjudge-7.2.0/submit -y -p firstbook -l " +
+          input.language +
+          " " +
+          filename;
+        let { stdout, stderr } = await exec(command);
+        const [, submit_id] = stderr.split("id = s");
+
+        //set submit_id command temporarily
+        command =
+          "python3 /home/ubuntu/code-at-once/analyze_submission/mapping_not_correct.py " +
+          (submit_id - 1).toString();
+        const { stdout: cmdout, stderr: cmderr } = await exec(command);
+        // console.log('stdout:', cmdout);
+        // console.error('stderr:', cmderr);
+        const result = cmdout.split("\n");
+
+        return {
+          problemId: input.problemId,
+          userId: input.userId,
+          language: input.language,
+          submitAt: new Date().toISOString(),
+          resultType: result[0],
+          message: result[1],
+        };
+      } catch (e) {
+        console.log(e);
+        return false;
+      }
+    },
+  },
+};
