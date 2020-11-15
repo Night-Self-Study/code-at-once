9d1db66d47cf6d7f5a388b8db5e3ea6178719ae5
solidw <solidw@naver.com>
2020-11-15 17:02:19 +0900

    style: Change Login, Register design

--- client/src/pages/auth/LoginPage.js
+++ client/src/pages/auth/LoginPage.js
@@ -6,25 +6,33 @@ import {
   makeStyles,
   Typography,
 } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 
 import CustomForm from '#/components/auth/CustomForm';
-import { useHistory } from 'react-router-dom';
+import { useUserContext } from '#/contexts/UserContext';
 
 const useStyles = makeStyles({
   root: {
     display: 'flex',
     height: '100%',
     justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: 'column',
   },
 });
 
 const LoginPage = () => {
   const classes = useStyles();
   const history = useHistory();
+  const { setUser } = useUserContext();
 
   const onClickLogin = () => {
     history.push('/');
+    setUser({
+      id: 'solidw',
+      password: 'p4ssword',
+      name: 'taewan',
+      email: 'asdf@asdf.com',
+    });
   };
 
   const onClickRegister = () => {
@@ -32,29 +40,27 @@ const LoginPage = () => {
   };
 
   return (
-    <Container className={classes.root} maxWidth='md'>
-      <div>
-        <Typography variant='h3'>로그인</Typography>
-        <CustomForm data={['ID', 'Password']} />
-        <Box py={2}>
-          <Button
-            fullWidth
-            variant='contained'
-            color='primary'
-            onClick={onClickLogin}
-          >
-            로그인
-          </Button>
-          <Button
-            fullWidth
-            variant='contained'
-            color='secondary'
-            onClick={onClickRegister}
-          >
-            회원가입
-          </Button>
-        </Box>
-      </div>
+    <Container className={classes.root} maxWidth='sm'>
+      <Typography variant='h3'>로그인</Typography>
+      <CustomForm data={['ID', 'Password']} />
+      <Box py={2}>
+        <Button
+          fullWidth
+          variant='contained'
+          color='primary'
+          onClick={onClickLogin}
+        >
+          로그인
+        </Button>
+        <Button
+          fullWidth
+          variant='contained'
+          color='secondary'
+          onClick={onClickRegister}
+        >
+          회원가입
+        </Button>
+      </Box>
     </Container>
   );
 };
--- client/src/pages/auth/RegisterPage.js
+++ client/src/pages/auth/RegisterPage.js
@@ -13,7 +13,7 @@ const useStyles = makeStyles({
   root: {
     display: 'flex',
     justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: 'column',
     height: '100%',
   },
 });
@@ -22,18 +22,16 @@ export default function RegisterPage() {
   const classes = useStyles();
 
   return (
-    <Container className={classes.root} maxWidth='md'>
-      <div>
-        <Typography variant='h3'>회원가입</Typography>
-        <CustomForm
-          data={['ID', 'Password', 'Password 확인', '이름', 'E-Mail']}
-        />
-        <Box py={2}>
-          <Button fullWidth variant='contained' color='secondary'>
-            회원가입
-          </Button>
-        </Box>
-      </div>
+    <Container className={classes.root} maxWidth='sm'>
+      <Typography variant='h3'>회원가입</Typography>
+      <CustomForm
+        data={['ID', 'Password', 'Password 확인', '이름', 'E-Mail']}
+      />
+      <Box py={2}>
+        <Button fullWidth variant='contained' color='secondary'>
+          회원가입
+        </Button>
+      </Box>
     </Container>
   );
 }
